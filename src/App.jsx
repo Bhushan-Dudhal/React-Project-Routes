9d50c851726c96d7af35a14792005fa42eea9915
Bhushan-Dudhal <bhushandhudhal9@gmail.com>
2024-05-07 19:10:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
